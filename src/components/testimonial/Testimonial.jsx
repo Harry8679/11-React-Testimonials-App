6c266032ebb148c29d959c9d1b5d89e09f3d0d45
Harry MacCode <emarh.harry.code@gmail.com>
2024-02-01 22:08:30 +0100

    Testimonial Component added

---
+++ src/components/testimonial/Testimonial.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Testimonial.scss';
+
+const Testimonial = () => {
+  return (
+    <h1>
+      Testimonials
+    </h1>
+  )
+}
+
+export default Testimonial;
---
+++ src/components/testimonial/data.js
@@ -0,0 +1,26 @@
+export const users = [
+  {
+    img: "https://i.ibb.co/RzVhF9f/image-tanya.jpg",
+    name: "Tanya Sinclair",
+    job: "UX Engineer",
+    desc: " “I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future. ”",
+  },
+  {
+    img: "https://i.ibb.co/L6vBVVw/image-john.jpg",
+    name: "John Tarkpor",
+    job: "Front-end Developer",
+    desc: " “ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”",
+  },
+  {
+    img: "https://img-c.udemycdn.com/user/200_H/42175526_ac8a.jpg",
+    name: "Ariel EBANG",
+    job: "Software Developer",
+    desc: " “I am a web, mobile and software developer. I am a Front and Back developer is Full Stack developer in each of the areas mentioned above. I am also a trainer in computer science and mathematics, physical and chemistry. ”",
+  },
+  {
+    img: "https://upload.wikimedia.org/wikipedia/commons/2/27/Stephen_Curry_Shooting_%28cropped%29.jpg",
+    name: "Steph CURRY",
+    job: "NBA Player",
+    desc: " “ Stephen Curry, born March 14, 1988 in Akron, is an American basketball player playing as a point guard for the Golden State Warriors team in the NBA.  Best three -point shooter in the history of the NBA.”",
+  }
+];
